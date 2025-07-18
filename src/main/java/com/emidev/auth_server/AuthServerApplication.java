60975d4bc2bb6a354bf7f59dc9c9d6c4fc3a11e2
darwinemilioreyesreyes <emiliorr2013@gmail.com>
2025-07-18 10:31:27 -0500

    registeredClients desde base de datos, configuracion de cors

--- src/main/java/com/emidev/auth_server/AuthServerApplication.java
+++ src/main/java/com/emidev/auth_server/AuthServerApplication.java
@@ -1,9 +1,12 @@
 package com.emidev.auth_server;
 
+import com.emidev.auth_server.web.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CorsProperties.class)
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,41 @@
+INSERT INTO oauth2_client (
+    client_id,
+    client_secret,
+    client_authentication_methods,
+    authorization_grant_types,
+    redirect_uris,
+    post_logout_redirect_uris,
+    scopes,
+    require_consent,
+    access_token_ttl_minutes,
+    refresh_token_ttl_minutes,
+    enabled
+) VALUES
+-- Cliente confidencial
+(
+    'client-app',
+    '{noop}12345',
+    'client_secret_basic',
+    'authorization_code,refresh_token',
+    'http://127.0.0.1:8081/login/oauth/code/client-app,http://127.0.0.1:8081/authorized',
+    'http://127.0.0.1:8081/logout',
+    'read,write,openid,profile',
+    false,
+    60,
+    43200,
+    true
+),
+-- Cliente público Angular
+(
+    'angular-client',
+    NULL,
+    'none',
+    'authorization_code,refresh_token',
+    'http://localhost:4200/authorized',
+    NULL,
+    'read,write,openid,profile',
+    false,
+    60,
+    43200,
+    true
+);
