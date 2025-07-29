732d6e19c7e0966a29f291270715e39cec587e4f
darwinemilioreyesreyes <emiliorr2013@gmail.com>
2025-07-29 16:01:10 -0500

    configuracion swagger, password encoder

--- src/main/java/com/emidev/auth_server/web/config/SwaggerConfig.java
+++ src/main/java/com/emidev/auth_server/web/config/SwaggerConfig.java
@@ -1,6 +1,11 @@
 package com.emidev.auth_server.web.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.http.HttpHeaders;
 
 
 @OpenAPIDefinition(
@@ -16,7 +21,18 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
         servers = @io.swagger.v3.oas.annotations.servers.Server(
                 url = "http://localhost:9000",
                 description = "Local server"
+        ),
+        security = @SecurityRequirement(
+                name = "Security token"
         )
+)
+@SecurityScheme(
+        name = "Security token",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in= SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 public class SwaggerConfig {
 }
